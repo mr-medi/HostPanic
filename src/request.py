7a7af6fabd38173779267284d4cfc32067c4c732
mr-medi <59095661+mr-medi@users.noreply.github.com>
2021-11-28 21:05:17 +0100

    Update request.py. Commenting and improving some functionality
    Commenting get_request function and his functionality.
    Improving get_request function.

--- src/request.py
+++ src/request.py
@@ -6,6 +6,17 @@ from src.utilities import *
 from bs4 import BeautifulSoup
 from src.style import *
 
+#---------------------------------------------------------------------------------------------
+# CUSTOM FUNCTION TO PERFORM RAW HTTP REQUEST WITH CUSTOM HEADERS USING SOCKETS
+#
+# @url str Domain where to create the socket
+# @path str Path of the URL where to make the request (Ex: /admin)
+# @port int Port where to create the Socket
+# @verbose bool Boolean to print or not more information about the request
+# @headers array Custom headers to send in the request
+# @tags array
+# @return None (For the moment this function doesn't return anything)
+#---------------------------------------------------------------------------------------------
 def get_request(url, path, port, verbose, headers=[], tags=[]):
     cookie = ""
     headers2 = ['Pragma: no-cache', 'Accept: */*', 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:86.0) Gecko/20100101 Firefox/86.0', 'Connection: close', 'Host: '+url]
@@ -36,7 +47,9 @@ def get_request(url, path, port, verbose, headers=[], tags=[]):
             if u_header[0] == "Cookie":
                 cookie = u_header[1]
 
-    # BUILD RAW HTTP REQUEST
+     #----------------------------------------------------------------------
+     # ------> Build the RAW HTTP REQUEST <---------------------------------
+     #----------------------------------------------------------------------
     host = ""
     xHost = ""
 
@@ -62,15 +75,18 @@ def get_request(url, path, port, verbose, headers=[], tags=[]):
         if name == "X-Forwarded-Host":
             xHost = val
 
+    if "http" in url:
+        url = urlparse(url).netloc
     request += 'Cookie: ' + cookie+"\r\n"
     request += "\r\n"
     print(request)
 
-    # SOCKET
+    # --> CREATE SOCKET TO THE TARGET <--
     try:
-        if "http" in url:
-            url = urlparse(url).netloc
-
+        #----------------------------------------------------------------------
+        # ------> Declaration of variables <----------------------------------
+        #----------------------------------------------------------------------
+        html = ""
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
         soc = context.wrap_socket(s, server_hostname=url)
@@ -79,44 +95,45 @@ def get_request(url, path, port, verbose, headers=[], tags=[]):
         soc.send(bytes(request, 'UTF-8'))
         result = soc.recv(2000)
         headersRes = []
-        html = ""
+        htmlRaw = ""
         rawHeadersResponse = str(result, "UTF-8")
         rawHeadersResponse = rawHeadersResponse.split("\r\n\r\n")[0]
 
-        # GET ALL HOST HEADER INJECTION IN HTTP HEADERS
+        #----------------------------------------------------------------------
+        # ------> GET ALL HOST HEADER INJECTION IN HTTP HEADERS <--------------
+        #----------------------------------------------------------------------
         positions = list(findall(host, rawHeadersResponse))
 
+        # Foreach Injection in HTTP Headers we print it
         for i in positions:
-            print(style.MAGENTA + '[ + ]' + style.RESET + "Found host injection in HTTP HEADER for " + style.UNDERLINE + host+ style.RESET)
             hostHTML = rawHeadersResponse[i:][:len(host)]
-            left = rawHeadersResponse[i - 90:][:90]
-            right = rawHeadersResponse[i + len(host):][:90]
+            if hostHTML != url:
+                print(style.MAGENTA + '[ + ]' + style.RESET + "Found host injection in HTTP HEADER for " + style.UNDERLINE + host+ style.RESET)
+                left = rawHeadersResponse[i - 90:][:90]
+                right = rawHeadersResponse[i + len(host):][:90]
 
-            print(left + style.GREEN + hostHTML + style.RESET + right + "\r\n")
-
+        # WHILE THE SERVER IS SENDING US THE RAW HTML WE PROCESS IT
         while (len(result) > 0):
             result = soc.recv(200)
-
-            soup = BeautifulSoup(result, 'html.parser')
+            soup = BeautifulSoup(result, 'html.parser',from_encoding="iso-8859-1")
             parse = soup.prettify()
             html = soup.prettify()
+            htmlRaw += str(html)
 
-            #if verbose:
-            #    print(html)
-            # GET ALL HOST HEADER INJECTION IN RESPONSE
+            # --------------> GET ALL HOST HEADER INJECTION IN THE BODY OF THE HTTP RESPONSE <--------------------
             if host not in url:
                 positions = list(findall(host, parse))
 
                 for i in positions:
-                    html = soup.prettify()
-                    print(style.MAGENTA + '[ + ]' + style.RESET + "Found host injection for " + style.UNDERLINE + soup.prettify()[i:][:len(host)] + style.RESET)
+                    html = soup.prettify()                    
                     hostHTML = html[i:][:len(host)]
                     left = html[i - 20:][:20]
                     right = html[i + len(host):][:40]
-
+                    
+                    print(style.MAGENTA + '[ + ]' + style.RESET + "Found host injection for " + style.UNDERLINE + soup.prettify()[i:][:len(host)] + style.RESET)
                     print(left + style.GREEN + hostHTML + style.RESET + right + "\r\n")
 
-            # X-FORWARDED-HOST HEADER INJECTION
+            # --------------> GET ALL X-FORWARDED-HOST HEADER INJECTION IN THE BODY OF THE HTTP RESPONSE <--------------------
             if xHost:
                 positions = list(findall(xHost, parse))
 
@@ -128,18 +145,18 @@ def get_request(url, path, port, verbose, headers=[], tags=[]):
 
                     print(style.MAGENTA + '[ + ]' + style.RESET + "Found host injection for " + style.UNDERLINE + soup.prettify()[i:][:len(xHost)] + style.RESET)
                     print(left + style.GREEN + hostHTML + style.RESET + right + "\r\n")
-
-
+            
+            # Finally close the Socket
             s.close()
 
         httpCode = 100
-        # If Verbose print HTTP Headers Response
+        # If Verbose Mode print HTTP Headers Response and the RAW HTML
         if verbose:
             headersRaw = str(rawHeadersResponse).split("\r\n")
             if len(str(rawHeadersResponse).split("\r\n")[0]) > 1:
                 httpCode = str(rawHeadersResponse).split("\r\n")[0].split(" ")[1]
-            print(style.GREEN + str(httpCode) +style.RESET)
-            print(html)
+            print("HTTP Code: " + style.GREEN + str(httpCode) + style.RESET + "\r\n")
+            print(htmlRaw)
 
             for header in headersRaw:
                 name = ""
@@ -157,154 +174,20 @@ def get_request(url, path, port, verbose, headers=[], tags=[]):
                     if header.split(':')[0] == "Location":
                         location = (header.split(':')[1]).replace(" ", "")
                         valid_url = location[0] == "/"
-                        wait = input("Press Enter to follow the request to "+location+":")
+                        wait = input("\r\nPress Enter to follow the request to " + location + " : \r\n")
+
                         if valid_url:
-                            get_request(url.replace(" ", ""), location, 443, verbose, ['Host:' + host])
-                            wait = input("Press Enter to FINISH")
+                            get_request(url.replace(" ", ""), location, 443, True, ['Host:' + host, 'Cookie:' + cookie])
+                            wait = input("-------------> Press Enter to FINISH <-----------------------\r\n")
         print("\r\n\r\n")
+        
+    # Manage possible exceptions because of the socket
     except socket.error as socketerror:
-        print(style.RED + "Error => "+style.RESET+str(socketerror))
+        print(style.RED + "Error => " + style.RESET + str(socketerror))
     except Exception as e:
         print(str(e))
-
-# BUILD HTTP REQUEST
-def doRequest(method, url, path, port, verbose, headers=[]):
-    headers2 = ['Pragma: no-cache', 'Accept: */*', 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:86.0) Gecko/20100101 Firefox/86.0', 'Connection: close', 'Host: '+url]
-    DEF = []
-
-    for h in headers2:
-        foundHeader = False
-        name = str(h).split(':')[0]
-        val = str(h).split(':')[1]
-
-        for userHeader in headers:
-            if name in userHeader:
-                foundHeader = True
-                u_header = userHeader.split(':')
-                if len(u_header) >= 3:
-                    DEF.append(u_header[0] + ": " + u_header[1]+":"+u_header[2])
-                else:
-                    DEF.append(u_header[0] + ": " + u_header[1])
-
-        # JUST A CHECK TO NOT REPEAT HTTP HEADERS
-        if not foundHeader:
-            DEF.append(h)
-
-    # BUILD RAW HTTP REQUEST
-    host = ""
-    xHost = ""
-
-    if not "http" in path and not "/" in path:
-        path = urlparse(url).path + "?" + urlparse(url).query
-    if path == "":
-        path = "/"
-
-    request = method + " " + path + " HTTP/1.1\r\n"
-
-    for i in DEF:
-        name = i.split(':')[0].strip()
-        val = i.split(':')[1].strip()
-
-        if len(i.split(':')) >= 3 :
-            val += ":"+i.split(':')[2]
-
-        lineHeader = name + ": " + val + "\r\n"
-        request += lineHeader
-
-        if name == "Host":
-            host = val
-        if name == "X-Forwarded-Host":
-            xHost = val
-
-    request += 'Cookie: ' + cookie+"\r\n"
-    request += "\r\n"
-    print(request)
-
-    # SOCKET
-    try:
-        if "http" in url:
-            url = urlparse(url).netloc
-
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-        soc = context.wrap_socket(s, server_hostname=url)
-        soc.settimeout(10)
-        soc.connect((url, port))
-        soc.send(bytes(request, 'UTF-8'))
-        result = soc.recv(500)
-        headersRes = []
-        html = ""
-        rawHeadersResponse = str(result, "UTF-8")
-
-        while (len(result) > 0):
-            result = soc.recv(200)
-
-            soup = BeautifulSoup(result, 'html.parser')
-            parse = soup.prettify()
-            html = soup.prettify()
-
-            if verbose:
-                print(html)
-
-            # GET ALL HOST HEADER INJECTION IN RESPONSE
-            positions = list(findall(host, parse))
-
-            for i in positions:
-                html = soup.prettify()
-                print(style.MAGENTA + '[ + ]' + style.RESET + "Found host injection for " + style.UNDERLINE + soup.prettify()[i:][:len(host)] + style.RESET)
-                hostHTML = html[i:][:len(host)]
-                left = html[i - 20:][:20]
-                right = html[i + len(host):][:40]
-
-                print(left + style.GREEN + hostHTML + style.RESET + right + "\r\n")
-
-            # X-FORWARDED-HOST HEADER INJECTION
-            if xHost:
-                positions = list(findall(xHost, parse))
-
-                for i in positions:
-                    html = soup.prettify()
-                    print(style.MAGENTA + '[ + ]' + style.RESET + "Found host injection for " + style.UNDERLINE + soup.prettify()[i:][:len(xHost)] + style.RESET)
-                    hostHTML = html[i:][:len(xHost)]
-                    left = html[i - 20:][:20]
-                    right = html[i + len(xHost):][:40]
-
-                    print(left + style.GREEN + hostHTML + style.RESET + right + "\r\n")
-
-
-            s.close()
-
-        # If Verbose print HTTP Headers Response
-        if verbose:
-            headersRaw = str(rawHeadersResponse).split("\r\n")
-            httpCode = str(rawHeadersResponse).split("\r\n")[0].split(" ")[1]
-            print(style.GREEN + httpCode +style.RESET)
-            for header in headersRaw:
-                name = ""
-                val = ""
-
-                if len(header.split(':')) > 1:
-                    name = header.split(':')[0]
-                    val = header.split(':')[1]
-                    if not "<html>" in name:
-                        print(style.YELLOW + name +style.RESET + ": " + val )
-
-        # Follow Redirects if User Want To
-        httpCode = str(rawHeadersResponse).split("\r\n")[0].split(" ")[1]
-        if int(httpCode) >= 300 and int(httpCode) <= 399:
-            for header in headersRaw:
-                if len(header.split(':')) > 1:
-                    if header.split(':')[0] == "Location":
-                        location = (header.split(':')[1]).replace(" ", "")
-                        wait = input("Press Enter to follow the request to "+location+":")
-                        get_prueba(url.replace(" ", ""), location, 443, verbose, ['Host:' + host])
-                        wait = input("Press Enter to FINISH")
-
-    except socket.error as socketerror:
-        print(style.RED + "Error => "+style.RESET+str(socketerror))
-    except Exception as e:
-        print(str(e))
-
+        
+        
 #----------------------------
 def getDiffTime(url, port, host):
 
